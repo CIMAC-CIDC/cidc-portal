5aaeb57871ee2c1157f12cddead0790ce3a5b086
mtmcduffie <mcduffie.michael@gmail.com>
2018-08-13 15:12:59 -0400

    dfci/cidc-portal#33 - Unrelated session error.

--- cidc_portal/main/services/utils.py
+++ cidc_portal/main/services/utils.py
@@ -15,17 +15,17 @@ def url_for_with_prefix(url: str):
 
 
 def base_user_info(session):
-    session["cidc_user_info"] = get_user_info(session["jwt_token"])
+    if "jwt_token" in session:
+        session["cidc_user_info"] = get_user_info(session["jwt_token"])
 
-    if session["cidc_user_info"] is None:
-        return dict()
-    else:
-        return dict(user_role=session["cidc_user_info"]["role"],
-                    user_name=session["cidc_user_info"]["username"],
-                    clinical_trials_status_url=url_for_with_prefix("/trials-summary"),
-                    cimac_biofx_home_url=url_for_with_prefix("/cimac_biofx/home"),
-                    admin_home_url=url_for_with_prefix("/admin/home"),
-                    privacy_url=url_for_with_prefix("/privacy"),
-                    logout_url=url_for_with_prefix("/logout"),
-                    user_info_url=url_for_with_prefix("/admin/user_info"),
-                    )
+        if session["cidc_user_info"] is not None:
+            return dict(user_role=session["cidc_user_info"]["role"],
+                        user_name=session["cidc_user_info"]["username"],
+                        clinical_trials_status_url=url_for_with_prefix("/trials-summary"),
+                        cimac_biofx_home_url=url_for_with_prefix("/cimac_biofx/home"),
+                        admin_home_url=url_for_with_prefix("/admin/home"),
+                        privacy_url=url_for_with_prefix("/privacy"),
+                        logout_url=url_for_with_prefix("/logout"),
+                        user_info_url=url_for_with_prefix("/admin/user_info"),
+                        )
+    return dict()
