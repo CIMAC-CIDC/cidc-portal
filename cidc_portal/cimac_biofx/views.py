2a72f16d595b4f6fd704118cf077b73f7faa9f36
Michael Tommie McDuffie <mcduffie.michael@gmail.com>
2018-11-06 08:47:01 -0500

    Update views.py

--- cidc_portal/cimac_biofx/views.py
+++ cidc_portal/cimac_biofx/views.py
@@ -7,6 +7,8 @@ from cidc_portal.auth.wrapper import requires_login, requires_roles
 
 from cidc_portal.main.services.user import get_user_info
 from cidc_portal.main.services.user import get_cimac_biofox_user_home_data
+from cidc_portal.main.services.uploads import get_olink_status
+from cidc_portal.main.services.uploads import remove_data_record
 from cidc_portal.main.services.utils import base_user_info
 
 from cidc_portal.main.services.utils import url_for_with_prefix
@@ -37,6 +39,21 @@ def home():
     )
 
 
+@cimac_biofx_bp.route("/cimac_biofx/uploads", methods=["GET"])
+@requires_login()
+@requires_roles([CIMAC_BIOFX_ROLE, ADMIN_ROLE])
+def uploads():
+    session["cidc_user_info"] = get_user_info(session["jwt_token"])
+
+    olink_uploads = get_olink_status(session["jwt_token"])
+
+    return render_template(
+        "upload_status.jinja2", jwt=session["jwt_token"],
+        olink_uploads=olink_uploads,
+        remove_uploaded_file_url=url_for_with_prefix("/cimac_biofx/remove-upload")
+    )
+
+
 @cimac_biofx_bp.route("/cimac_biofx/cli-install", methods=["GET"])
 @requires_login()
 @requires_roles([CIMAC_BIOFX_ROLE, ADMIN_ROLE])
@@ -57,3 +74,16 @@ def olink_upload():
     return render_template(
         "olink-upload.jinja2", jwt=session["jwt_token"]
     )
+
+
+@cimac_biofx_bp.route("/cimac_biofx/remove-upload", methods=["POST"])
+@requires_login()
+@requires_roles([CIMAC_BIOFX_ROLE, ADMIN_ROLE])
+def remove_upload():
+    session["cidc_user_info"] = get_user_info(session["jwt_token"])
+
+    data_id = request.form.get("data_id")
+
+    remove_data_record(session["jwt_token"], data_id)
+
+    return redirect(url_for_with_prefix("/cimac_biofx/uploads"))
