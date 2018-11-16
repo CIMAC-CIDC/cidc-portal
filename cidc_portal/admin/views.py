eeee49ad3a7ba11ba3d638765f6ec19195e55b71
mtmcduffie <mcduffie.michael@gmail.com>
2018-11-16 13:08:20 -0500

    dfci/cidc-portal#91 - Updating admin portal with new auth changes.

--- cidc_portal/admin/views.py
+++ cidc_portal/admin/views.py
@@ -15,6 +15,7 @@ from cidc_portal.main.services.admin import fetch_single_user
 from cidc_portal.main.services.admin import change_user_role
 from cidc_portal.main.services.admin import add_user_to_trial
 from cidc_portal.main.services.admin import remove_user_from_trial
+from cidc_portal.main.services.assays import fetch_assays
 
 from cidc_portal.main.forms.registration import RegistrationForm
 
@@ -22,6 +23,8 @@ from cidc_portal.main.services.utils import url_for_with_prefix
 
 from constants import ADMIN_ROLE
 from constants import ROLE_LIST
+
+from cidc_portal.main.services.admin import TRIAL_ASSAY_PERMISSION_TYPES
 
 admin_bp = Blueprint("admin", __name__, template_folder="templates")
 
@@ -44,8 +47,10 @@ def home():
 @requires_login()
 @requires_roles([ADMIN_ROLE])
 def user_info():
-
     selected_user = request.args.get("user_id")
+
+    all_trials = fetch_trials(session["jwt_token"])
+    all_assays = fetch_assays(session["jwt_token"])
 
     retrieved_user_info = fetch_single_user(session["jwt_token"], selected_user)
 
@@ -56,31 +61,43 @@ def user_info():
         first_n=retrieved_user_info.get("first_n"),
         last_n=retrieved_user_info.get("last_n"),
         cidc_role=retrieved_user_info.get("position_description"),
+        permissions=retrieved_user_info.get("permissions"),
     )
 
+    # Fetch trials the user is a collaborator on.
     users_trials = fetch_users_trials(
         session["jwt_token"], retrieved_user_info["email"]
     )["_items"]
-    trials_user_not_in = fetch_trials(session["jwt_token"])
 
-    # Create a list of trials that the user is not in.
-    for user_trial in users_trials:
-        trials_user_not_in = [
-            trial
-            for trial in trials_user_not_in
-            if trial.get("_id") != user_trial["_id"]
-        ]
+    # Build a dict with the fancy names for the objects that make up permissions.
+    permission_display = []
+
+    for permission in retrieved_user_info["permissions"]:
+        trial_name = next(
+            (trial for trial in all_trials if trial["_id"] == permission["trial"]), None
+        )["trial_name"]
+
+        assay_name = next(
+            (assay for assay in all_assays if assay["_id"] == permission["assay"]), None
+        )["assay_name"]
+
+        permission_display.append(
+            {"trial": trial_name, "assay": assay_name, "role": permission["role"]}
+        )
 
     return render_template(
         "admin_user_info.jinja2",
         retrieved_user_info=retrieved_user_info,
         register_form=register_form,
-        trials_user_not_in=trials_user_not_in,
         users_trials=users_trials,
         role_list=ROLE_LIST,
         update_role_url=url_for_with_prefix("/admin/update_role"),
         add_user_to_trial_url=url_for_with_prefix("/admin/add_user_trial"),
         remove_user_from_trial_url=url_for_with_prefix("/admin/remove_user_from_trial"),
+        trial_assay_permission_types=TRIAL_ASSAY_PERMISSION_TYPES,
+        assays=all_assays,
+        all_trials=all_trials,
+        permissions_display=permission_display,
     )
 
 
@@ -102,8 +119,12 @@ def update_role():
 def add_user_trial():
     user_id = request.form.get("user_id")
     new_trial = request.form.get("add_user_to_trial")
+    assay_type = request.form.get("assay_type")
+    trial_permission = request.form.get("trial_permission")
 
-    add_user_to_trial(session["jwt_token"], new_trial, [user_id])
+    add_user_to_trial(
+        session["jwt_token"], new_trial, [user_id], assay_type, trial_permission
+    )
 
     return redirect(url_for_with_prefix("/admin/user_info?user_id=%s" % user_id))
 
